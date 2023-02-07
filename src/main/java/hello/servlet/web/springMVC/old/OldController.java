3527f59226cb000939e38748e7af709cf4b640da
mingyu <mingyu.oh15b@gmail.com>
2023-02-07 16:28:09 +0900

    springMVC - 핸들러와 핸들러매핑과 핸들러어뎁터

---
+++ src/main/java/hello/servlet/web/springMVC/old/OldController.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.springMVC.old;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+//hadlerAdpater => simpleControllerHandlerAdapter()
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("oldController.handleRequest");
+        System.out.println(request.getMethod());
+
+        return null;
+    }
+}
